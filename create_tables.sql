e980179b3f7b066a6700accf6f0acae68f05e6e9
Adam <adam.michael.streff@gmail.com>
2024-04-01 13:30:45 -0400

    sql ddl

---
+++ create_tables.sql
@@ -0,0 +1,38 @@
+USE dataBASED;
+
+CREATE TABLE Artist (
+    id INT PRIMARY KEY,
+    name VARCHAR(255),
+    birth_year INT,
+    death_year INT,
+    biography TEXT
+);
+
+CREATE TABLE Artwork
+(
+    id              INT PRIMARY KEY,
+    title           TEXT,
+    start_year      INT,
+    end_year        INT,
+    place_of_origin VARCHAR(255),
+    image_id        VARCHAR(36),
+    color           VARCHAR(100),
+    colorfulness    DECIMAL(8, 4),
+    not_viewed_much BOOLEAN
+);
+
+CREATE TABLE Descriptor (
+    artwork_id INT,
+    FOREIGN KEY (artwork_id) REFERENCES Artwork(id),
+    aspect ENUM('artwork_type', 'category', 'classification', 'material', 'style', 'technique') NOT NULL,
+    title TEXT
+);
+
+CREATE TABLE Made (
+    artist_id INT,
+    artwork_id INT,
+    PRIMARY KEY (artist_id, artwork_id),
+    FOREIGN KEY (artist_id) REFERENCES Artist(id),
+    FOREIGN KEY (artwork_id) REFERENCES Artwork(id)
+);
+

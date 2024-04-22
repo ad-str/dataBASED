a774db3d72e9f3f5b6075bacd095b8218a5f6392
Adam <adam.michael.streff@gmail.com>
2024-04-22 13:08:30 -0400

    prettifying it

--- client/src/pages/ArtAtlas.jsx
+++ client/src/pages/ArtAtlas.jsx
@@ -9,14 +9,12 @@ const url = `http://${config.server_host}:${config.server_port}`;
 
 export default function ArtAtlas() {
   const [artworks, setArtworks] = useState([]);
-
-  // useEffect(() => {
-  //   fetch(`${url}/artworks-location/united`)
-  //     .then((res) => res.json())
-  //     .then((resJson) => setLocations(resJson));
-  // }, []);
+  const [activeCountry, setActiveCountry] = useState("");
+  const [hoveredCountry, setHoveredCountry] = useState("");
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
   const handleCountryClick = async (countryName) => {
+    setActiveCountry(countryName);
     try {
       const response = await axios.get(`${url}/map/${countryName}`);
       setArtworks(response.data);
@@ -25,74 +23,47 @@ export default function ArtAtlas() {
     }
   };
 
+  const countryStyles = (countryName) => ({
+    default: { 
+      fill: countryName === activeCountry ? "#E42" : "#D6D6DA", // Active country gets a different fill
+      stroke: "#FFFFFF", 
+      strokeWidth: 0.75 
+    },
+    hover: { 
+      fill: "#F53", 
+      stroke: "#FFFFFF", 
+      strokeWidth: 1.5 
+    },
+    pressed: { 
+      fill: "#E42", 
+      stroke: "#FFFFFF", 
+      strokeWidth: 1.5 
+    }
+  });  
+
+  const handleMouseMove = (event) => {
+    const mapContainer = event.currentTarget; // Get the reference to the map container
+    const rect = mapContainer.getBoundingClientRect(); // Get bounding rectangle of the map container
+  
+    setMousePosition({
+      x: event.clientX - rect.left, // Subtract the left position of the container
+      y: event.clientY - rect.top  // Subtract the top position of the container
+    });
+  };
+  
+
+  const handleMouseEnter = (countryName) => {
+    setHoveredCountry(countryName);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredCountry("");
+  };
+
   return (
-    <>
-      <h2>What's beautiful about getting to know art through a map?</h2>
-      <h3>Unveiling hidden connections:</h3>
-      <p>
-        Identifying specific artists across geographical locations can reveal
-        surprising connections, perhaps a shared or the evolution of a style as
-        it traveled through borders.
-      </p>
-      <h3>A new perspective on art history:</h3>
-      <p>
-        Maps can challenge the traditional, Eurocentric view of art history. By
-        showcasing art from all corners of the world, it allows you to see the
-        richness and diversity of artistic expression throughout time.
-      </p>
-      <h3>Planning your own art adventure:</h3>
-      <p>
-        See artistic hubs you might not have known abou and build a unique
-        travel experince where you can follow a trail of art across specific
-        region{" "}
-      </p>
-
-      {/*  Commented out temporarily
-      <div>
-        <h4>Artworks from the United States:</h4>
-        <ul>
-          {locations.map((location, index) => (
-            <li key={index}>{location.name}</li>
-          ))}
-        </ul>
-      </div> */}
-
-      {/* <h2>View different artwork globally on our interactive map!</h2> */}
-
-      {/* 
-      //TODO 
-      Necessary:
-      -make Name of country text slightly more to left or addd hover feature for name 
-      -add image url to either hover or display to the right and left of the map
-      -add css styling to color map a preferred colorm, can reference all elements by className = allPaths
-      
-      Nice to have:
-      -Find out how to make map bigger and allow a user to scroll down to use
-
-      */}
-
-      <div id="name">
-        <h2 id="countryName">Name of Country :</h2>
-        <h2 id="countryArtworks">Artworks :
-          {artworks.map((artwork) => (
-            <div key={artwork.id}>
-              <img
-                src={`https://www.artic.edu/iiif/2/${artwork.image_id}/full/200,/0/default.jpg`}
-                alt={`${artwork.title} Artwork`}
-              />
-              <h4 key={artwork.id}>
-                <NavLink to={`/artwork/${artwork.id}`}>{artwork.title}</NavLink>
-              </h4>
-            </div>
-          ))}
-        </h2>
-      </div>
-
-      {/* <div id="timeCont">
-            <p id="time">Time</p>
-          </div> */}
-
-      <div>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'start', height: '80vh' }}>
+      {/* The map container */}
+      <div style={{ flex: 3, position: 'relative' }} onMouseMove={handleMouseMove}>
         <ComposableMap>
           <Geographies geography="/features.json">
             {({ geographies }) =>
@@ -101,12 +72,48 @@ export default function ArtAtlas() {
                   key={geo.rsmKey}
                   geography={geo}
                   onClick={() => handleCountryClick(geo.properties.name)}
+                  onMouseEnter={() => handleMouseEnter(geo.properties.name)}
+                  onMouseLeave={handleMouseLeave}
+                  style={countryStyles(geo.properties.name)}
                 />
               ))
             }
           </Geographies>
         </ComposableMap>
+        {hoveredCountry && (
+          <div style={{
+            position: 'absolute',
+            left: `${mousePosition.x + 10}px`,
+            top: `${mousePosition.y + 10}px`,
+            pointerEvents: 'none',
+            zIndex: 9999,
+            padding: '5px',
+            background: 'white',
+            border: '1px solid black',
+            borderRadius: '3px',
+            fontSize: '0.9em'
+          }}>
+            {hoveredCountry}
+          </div>
+        )}
       </div>
-    </>
+
+      {/* The artworks container */}
+      <div style={{ flex: 1, margin: '1rem', overflowY: 'auto', maxHeight: '80vh'}}>
+        <h2 id="countryArtworks">Artworks:</h2>
+        {artworks.map((artwork) => (
+          <div key={artwork.id}>
+            <img
+              src={`https://www.artic.edu/iiif/2/${artwork.image_id}/full/200,/0/default.jpg`}
+              alt={`${artwork.title} Artwork`}
+            />
+            <h4 key={artwork.id}>
+              <NavLink to={`/artwork/${artwork.id}`}>{artwork.title}</NavLink>
+            </h4>
+          </div>
+        ))}
+      </div>
+
+    </div>
   );
 }

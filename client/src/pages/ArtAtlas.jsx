7ca22839824eb632ab0c90a77d03f59c0ce0e3fc
Adam <adam.michael.streff@gmail.com>
2024-04-22 13:10:21 -0400

    commented out old content

--- client/src/pages/ArtAtlas.jsx
+++ client/src/pages/ArtAtlas.jsx
@@ -7,6 +7,27 @@ import config from "../config.json";
 
 const url = `http://${config.server_host}:${config.server_port}`;
 
+// old content, unsure if we want to keep:
+// <h2>What's beautiful about getting to know art through a map?</h2>
+//       <h3>Unveiling hidden connections:</h3>
+//       <p>
+//         Identifying specific artists across geographical locations can reveal
+//         surprising connections, perhaps a shared or the evolution of a style as
+//         it traveled through borders.
+//       </p>
+//       <h3>A new perspective on art history:</h3>
+//       <p>
+//         Maps can challenge the traditional, Eurocentric view of art history. By
+//         showcasing art from all corners of the world, it allows you to see the
+//         richness and diversity of artistic expression throughout time.
+//       </p>
+//       <h3>Planning your own art adventure:</h3>
+//       <p>
+//         See artistic hubs you might not have known abou and build a unique
+//         travel experince where you can follow a trail of art across specific
+//         region{" "}
+//       </p>
+
 export default function ArtAtlas() {
   const [artworks, setArtworks] = useState([]);
   const [activeCountry, setActiveCountry] = useState("");

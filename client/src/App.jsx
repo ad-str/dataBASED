da709e0c44e6452ff11114d89cd1bd9ef55bd664
Brianna Malcolm <brimalco@seas.upenn.edu>
2024-04-25 13:11:14 -0400

    navbar actually navigates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -69,24 +69,27 @@ export default App;
 */
 
 import { NavBar } from "./components/NavBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// import pages
+import HomePage from "./pages/HomePage";
+import ArtAtlas from "./pages/ArtAtlas";
+import Nameless from "./pages/Nameless";
+import StealLike from "./pages/StealLike";
+import Welcome from "./pages/Welcome";
 
 function App() {
   return (
-    <div className="min-h-screen flex justify-center p-5 items-center">
-      <NavBar />
-      <div className="space-y-5 text-center gap-5">
-        <h1 className="lg:text-4xl text-2xl  font-bold">
-          Welcome to ArtBased
-        </h1>
-        <p className="max-w-lg text-sm leading-6">
-          This was created to highlight the countless artists that don't get the recognition 
-          they deserve. We see you and we honor you.
-        </p>
-        <button className="px-5 py-3 bg-neutral-300 rounded-md text-sm text-stone-800">
-          Get Started
-        </button>
-      </div>
-    </div>
+    <BrowserRouter>
+        <NavBar />
+          <Routes>
+            <Route path="/" element={<Welcome />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/map" element={<ArtAtlas />} />
+            <Route path="/nameless" element={<Nameless />} />
+            <Route path="/steal" element={<StealLike />} />
+          </Routes>
+      </BrowserRouter>
   );
 }
 
--- client/src/pages/Welcome.jsx
+++ client/src/pages/Welcome.jsx
@@ -6,11 +6,11 @@ function Welcome() {
       <NavBar />
       <div className="space-y-5 text-center gap-5">
         <h1 className="lg:text-4xl text-2xl  font-bold">
-          Animated Hamburger Menu
+          Welcome to ArtBased
         </h1>
         <p className="max-w-lg text-sm leading-6">
-          This is a simple example of how to build an animate a hamburger menu
-          with React and Framer Motion. I hope you like it!
+          This was created to highlight the countless artists that don't get the recognition 
+          they deserve. We see you and we honor you.
         </p>
         <button className="px-5 py-3 bg-neutral-300 rounded-md text-sm text-stone-800">
           Get Started

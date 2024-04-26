19b8ec227c0df30a252ab743e64e81730d186796
Adam <adam.michael.streff@gmail.com>
2024-04-26 16:29:52 -0400

    fixed font size

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -5,7 +5,7 @@ export const NavBar = () => {
   return (
     <div className="p-3 shadow-lg bg-purple-300 border-b border-bg-gray-300">
       <nav className="container flex items-center justify-between py-2 lg:py-5">
-        <span className="text-4xl font-extrabold leading-none tracking-tight">
+        <span className="text-2xl font-extrabold leading-none tracking-tight">
           ArtBased⚡️
         </span>
         <NavMobile />

d347be281adca8bb07fc16ffee8cac693ee4b812
daunelaugust <daunelaugustin@gmail.com>
2024-04-26 15:53:07 -0400

    fixed artist card format

--- client/src/components/ArtistCard.jsx
+++ client/src/components/ArtistCard.jsx
@@ -1,10 +1,7 @@
-import { useEffect, useState } from "react";
-import { Box, Button, ButtonGroup, Modal } from "@mui/material";
-import { NavLink } from "react-router-dom";
-import config from "../config.json";
+import { Box, Button, Modal } from "@mui/material";
 
 
-export default function ArtistCard({artistInfo, handleClose }) {
+export default function ArtistCard({ artistInfo, handleClose }) {
 
   return (
     <Modal
@@ -13,7 +10,6 @@ export default function ArtistCard({artistInfo, handleClose }) {
       style={{
         display: "flex",
         justifyContent: "center",
-        // alignItems: "center",
       }}
     >
       <Box
@@ -22,16 +18,22 @@ export default function ArtistCard({artistInfo, handleClose }) {
           background: "white",
           borderRadius: "16px",
           border: "2px solid #000",
-          width: 600,
-          height: "auto",
-          justifyContent: "center",
-
+          width : "1000px",
+          height : "500px",
+          marginTop : "100px"
         }}
       >
         
         <div>
-        <h2 className="text-2xl font-style:font-bold dark:text-black"> <b>Artist</b> {artistInfo.name}</h2>
-         <p className="font-normal text-gray-500"> <b>Bio:</b> {artistInfo.biography}</p>
+          <h2 className="text-2xl font-style:font-bold dark:text-black">
+            <b>Artist:</b> {artistInfo.name}
+          </h2>
+          <p
+            className="font-normal text-gray-500"
+            style={{ marginTop: "10px" }} // Add margin between name and bio
+          >
+            <b>Biography:</b> {artistInfo.biography}
+          </p>
         </div>
         <Button onClick={handleClose}>Close</Button>
       </Box>
